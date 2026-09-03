022ca2346f96ec13c0b40e971a807222019f8a73
JayasaiKarthik-G <jayasaikarthik66@gmail.com>
2026-09-03 23:26:21 +0530

    add reviews

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -5,16 +5,57 @@ export const reviews = [
   //
   {
     id: 'review-01',
-    name: 'Customer Name',
+    name: 'Himavanth JP',
     rating: 5,
-    text: 'Actual Google review text...',
-    date: '2 months ago',
-    category: 'Wedding',
+    text: 'One of the best photography teams. CED – Capture Edit Deliver captured every moment perfectly with great quality and creativity. The team was very professional and friendly. Highly recommended for weddings and events.',
+    date: '5 months ago',
+    category: 'Event',
   },
+  {
+    id: 'review-02',
+    name: 'Erugu Sagar',
+    rating: 5,
+    text: 'Your creativity, editing and dedication helped us showcase our café in the best way possible. Every reel looks more professional and engaging because of your work! 🙌 Highly recommended for anyone looking for quality content creation.',
+    date: '1 week ago',
+    category: 'Event',
+  },
+  {
+    id: 'review-03',
+    name: 'Madhuri Band',
+    rating: 5,
+    text: 'Amazing experience with CED! The Reel was captured beautifully and delivered instantly. The quality, creativity, and attention to detail were outstanding. Highly recommended! 👌✨',
+    date: '5 months ago',
+    category: 'Event',
+  },
+  {
+    id: 'review-04',
+    name: 'G Venkatesh',
+    rating: 5,
+    text: 'Great work by ced team they have a good patience with lots of good creative thoughts truely appreciated their instant reels within 30 min they have gave us 20 reels it’s truly great',
+    date: '5 months ago',
+    category: 'Event',
+  },
+  {
+    id: 'review-05',
+    name: 'Mohd Mateen',
+    rating: 5,
+    text: 'We just booked a slot with them shooter came editd on spot and deliverd reel in min we are so happy for events book ced',
+    date: '2 weeks ago',
+    category: 'Event',
+  },
+  {
+    id: 'review-06',
+    name: 'Siddharth',
+    rating: 5,
+    text: 'One of the best instant Reel delivery in Hyderabad and Am really amazed by the quality and precision of taking . I strongly recommend 👌 👍',
+    date: '5 months ago',
+    category: 'Event',
+  },
+  
 ]
 
 export const reviewSummary = {
   rating: 5,
-  totalReviews: 0,
-  googleUrl: '',
+  totalReviews: 5,
+  googleUrl: 'https://share.google/lt7VkbDcMPUw91jdO',
 }
